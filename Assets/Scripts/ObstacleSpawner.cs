1d699e111cb0eddb0c943ac736fba72ece3050d6
chuhieu1 <chuhieuls123@gmail.com>
2022-04-19 00:04:54 +0700

    first commit

---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour {
+
+	[SerializeField] private float waitTime;
+    //[SerializeField] private GameObject[] obstaclePrefabs;
+    [SerializeField] private ObstacleBehaviour obstaclePrefab;
+	[SerializeField] private float minObstacleY;
+	[SerializeField] private float maxObstacleY;
+	[SerializeField] private float highDifference;
+	[SerializeField] private int easy;
+	[SerializeField] private int hard;
+	private float tempTime;
+	private ObstacleBehaviour pipeClone;
+	private int differenceIndex;
+
+	void Start(){
+		tempTime = waitTime - Time.deltaTime;
+	}
+
+	void LateUpdate () {
+		if(GameManager.Instance.GameState()){
+			tempTime += Time.deltaTime;
+			if(tempTime > waitTime){
+				// Wait for some time, create an obstacle, then set wait time to 0 and start again
+				tempTime = 0;
+				float posY = GetRandomPosY();
+				pipeClone = Instantiate(obstaclePrefab, transform.position + new Vector3(0f, posY), transform.rotation);
+
+			}
+		}
+	}
+
+	private float GetRandomPosY()
+	{
+		differenceIndex++;
+		if (pipeClone == null)
+		{
+			return Random.Range(minObstacleY, maxObstacleY);
+		}
+
+		if (differenceIndex > easy)		//easy position: index > 0, hard position: index <= 0
+        {
+			differenceIndex = -hard + 1;
+        }
+
+		float pipeY = pipeClone.transform.position.y;
+		if (differenceIndex > 0)
+        {
+			float minY = pipeY - highDifference;
+			if (minY < minObstacleY)
+			{
+				minY = minObstacleY;
+			}
+
+			float maxY = pipeY + highDifference;
+			if (maxY > maxObstacleY)
+            {
+				maxY = maxObstacleY;
+            }
+			return Random.Range(minY, maxY);
+		}
+		else
+        {
+			float min1Y = minObstacleY;
+			float max1Y = pipeY - highDifference;
+			float min2Y = pipeY + highDifference;
+			float max2Y = maxObstacleY;
+
+			if (max1Y < min1Y)
+            {
+				max1Y = min1Y;
+			}
+			if (min2Y > max2Y)
+            {
+				min2Y = max2Y;
+			}
+			if (min1Y == max1Y && min2Y == max2Y)
+			{
+				return Random.Range(minObstacleY, maxObstacleY);
+			}
+			else if (min1Y == max1Y)
+			{
+				return Random.Range(min2Y, max2Y);
+			}
+			else if (min2Y == max2Y)
+			{
+				return Random.Range(min1Y, max1Y);
+			}
+			return RandomWith2PairMinMax(min1Y, max1Y, min2Y, max2Y);
+		}
+	}
+
+	private float RandomWith2PairMinMax(float min1, float max1, float min2, float max2)
+    {
+		float result = Random.Range(min1, max1 + max2 - min2);
+		if (result > max1)
+        {
+			result = result - max1 + min2;
+        }
+		return result;
+    }
+
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.gameObject.transform.parent != null){
+			Destroy(col.gameObject.transform.parent.gameObject);
+		}else{
+			Destroy(col.gameObject);
+		}
+	}
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] private float thrust, minTiltSmooth, maxTiltSmooth, hoverDistance, hoverSpeed;
+	private bool start;
+	private float timer, tiltSmooth, y;
+    private Rigidbody2D playerRigid;
+    private Quaternion downRotation, upRotation;
+	private float velocity, time;
+
+	void Start () {
+		tiltSmooth = maxTiltSmooth;
+        playerRigid = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler (0, 0, -90);
+		upRotation = Quaternion.Euler (0, 0, 35);
+	}
+
+	void Update () {
+		if (!start) {
+			// Hover the player before starting the game
+			timer += Time.deltaTime;
+			y = hoverDistance * Mathf.Sin (hoverSpeed * timer);
+			transform.localPosition = new Vector3 (0, y, 0);
+		} else {
+			// Rotate downward while falling
+			transform.rotation = Quaternion.Lerp (transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+		}
+		// Limit the rotation that can occur to the player
+		transform.rotation = new Quaternion (transform.rotation.x, transform.rotation.y, Mathf.Clamp (transform.rotation.z, downRotation.z, upRotation.z), transform.rotation.w);
+	}
+
+	void LateUpdate () {
+		if (GameManager.Instance.GameState ()) {
+			if (Input.GetMouseButtonDown (0)) {
+				if(!start){
+					// This code checks the first tap. After first tap the tutorial image is removed and game starts
+					start = true;
+					GameManager.Instance.GetReady ();
+					GetComponent<Animator>().speed = 2;
+				}
+				//playerRigid.gravityScale = 1f;
+				tiltSmooth = minTiltSmooth;
+				transform.rotation = upRotation;
+				//playerRigid.velocity = Vector2.zero;
+				// Push the player upwards
+				//playerRigid.AddForce (Vector2.up * thrust);
+				time = 0;
+				velocity = 0.02f;
+				SoundManager.Instance.PlayTheAudio("Flap");
+			}
+		}
+		//if (playerRigid.velocity.y < -1f) {
+		//if (velocity < -1f)
+		//{
+		//	// Increase gravity so that downward motion is faster than upward motion
+		//	tiltSmooth = maxTiltSmooth;
+		//	//playerRigid.gravityScale = 2f;
+		//	velocity = -2f;
+		//}
+		if (velocity > 0)
+        {
+			time += Time.deltaTime;
+			if (time > 0.5)
+            {
+				velocity = -0.04f;
+				tiltSmooth = maxTiltSmooth;
+			}
+        }
+		else
+        {
+			tiltSmooth = maxTiltSmooth;
+		}
+		playerRigid.velocity = Vector2.zero;
+		transform.position += new Vector3(0f, velocity);
+    }
+
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.transform.CompareTag ("Score")) {
+			Destroy (col.gameObject);
+			GameManager.Instance.UpdateScore ();
+		} else if (col.transform.CompareTag ("Obstacle")) {
+			// Destroy the Obstacles after they reach a certain area on the screen
+			foreach (Transform child in col.transform.parent.transform) {
+				child.gameObject.GetComponent<BoxCollider2D> ().enabled = false;
+			}
+			KillPlayer ();
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D col) {
+		if (col.transform.CompareTag ("Ground")) {
+            playerRigid.simulated = false;
+            KillPlayer ();
+			transform.rotation = downRotation;
+		}
+	}
+
+	public void KillPlayer () {
+		GetComponent<Animator>().Play("PlayerDead");
+		GameManager.Instance.EndGame ();
+		velocity = 0;
+		//playerRigid.velocity = Vector2.zero;
+		// Stop the flapping animation
+		//GetComponent<Animator> ().enabled = false;
+	}
+
+}
